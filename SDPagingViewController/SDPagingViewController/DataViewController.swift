aba3f38a23fff1a68bae4425bc71d937c852cdc1
SvD <sergey@u4g.com>
2015-12-09 21:19:18 +0200

    PageViewController

---
+++ SDPagingViewController/SDPagingViewController/DataViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DataViewController.swift
+//  SDPagingViewController
+//
+//  Created by SvD on 09.12.15.
+//  Copyright © 2015 SvD. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+//        parentViewController?.parentViewController?.
+        title = dataObject
+    }
+
+
+}
+
