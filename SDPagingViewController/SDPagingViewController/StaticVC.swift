fc52620ff8cf8864fe586cbf9aecd0b3ff331ec6
Sergey <Dergach@MacBook-Pro-Sergey.local>
2015-12-11 11:51:09 +0200

    Experiments with params

--- SDPagingViewController/SDPagingViewController/StaticVC.swift
+++ SDPagingViewController/SDPagingViewController/StaticVC.swift
@@ -27,15 +27,15 @@ class StaticVC: UIViewController, SDLoopPickerDelegate {
 
 
 
-        let isp2 = SDLoopPicker(frame: CGRectMake(0, 60, 300, 120))
+        let isp2 = SDLoopPicker(frame: CGRectMake(0, 60, 300, 200))
 
         var centerPoint = isp2.center
         centerPoint.x = view.center.x
         isp2.center = centerPoint
         isp2.imageAry = imgSet
-        isp2.heightOffset = 30.0
-        isp2.positionRatio = 2.0
-        isp2.alphaOfobjs = 0.3
+        isp2.heightOffset = 40.0
+        isp2.positionRatio = 1.0
+        isp2.alphaOfobjs = 0.9
 //        isp2. setSelectedItem:0];
         view.addSubview(isp2)
 
